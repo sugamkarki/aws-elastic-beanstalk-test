6cee63065ecdf55ae29844a8c7376d785f603ebc
magusikrak <sugamkarki7058@gmail.com>
2021-12-05 09:46:17 +0545

    added basic api and uses swagger api

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('API TEST')
+    .setDescription('The something description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.port || 3000);
+}
+bootstrap();
